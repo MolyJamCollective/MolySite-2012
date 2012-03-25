77acefab8173811f3e8e3dd73be68ec5e07e9911
Zane Wolfgang Pickett <sirwolf@frostedwolf.com>
2012-03-25 18:56:17 -0400

    Fixed Email formatting and data
    Signed-off-by: Zane Wolfgang Pickett <sirwolf@frostedwolf.com>

--- sendConfirmationEmail.php
+++ sendConfirmationEmail.php
@@ -1,6 +1,6 @@
 <?php
 
-	function SendConfirmationEmail( $mail )
+	function SendConfirmationEmail( $mail, $Game )
     {
     	$mailer = new PHPMailer();
 		$mailer->AddReplyTo( "noreply@whatwouldmolydeux.com", "MolyJam 2012" );
@@ -8,18 +8,17 @@
 		$mailer->AddAddress( $mail );
 		$mailer->Subject    = "MolyGame 2012 Game Submission";
 		
-		$text = "Dear MolyJamer,
-
-Thank you for sharing your project!
-
-Below is a link to allow you to edit your entry, this link will only work for 24 hours, after which your game will be stored forever in carbonite.
-
-http://www.whatwouldmolydeux.com/submit.php?EditID=" . $Game->EditID . "
-
-Sincerely,
-
-The Molyjam Organizer Collective.";
-		
+		$text = "Dear MolyJamer,<br />
+<br />
+Thank you for sharing your project!<br />
+<br />
+Below is a link to allow you to edit your entry, this link will only work for 24 hours, after which your game will be stored forever in carbonite.<br />
+<br />
+http://www.whatwouldmolydeux.com/submit.php?EditID=" . $Game->EditID . "<br />
+<br />
+Sincerely,<br />
+<br />
+The Molyjam Organizer Collective.<br />";
 		$mailer->MsgHTML( $text );
 		$mailer->Send();
    	}
