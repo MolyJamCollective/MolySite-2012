e6d86a21c62e5bbb5aa0a6208783f1fce1f70fb9
OliverEberlei <oliver@eberlei.de>
2012-03-26 02:33:43 +0200

    Added Dieburg to locations

--- data.php
+++ data.php
@@ -1,7 +1,7 @@
 <?php
 
     // Since Locations are stored as text in the SQL, there is no dependency on order
-    $Locations = array("Austin", "Boston", "Brighton", "Christchurch", "Dundee",
+    $Locations = array("Austin", "Boston", "Brighton", "Christchurch", "Dieburg", "Dundee",
                        "Las Vegas","Lexington", "Lisbon", "London", "Los Angeles",
                        "Malmo", "Melbourne", "Monterrey", "Montreal", "Munich",
                        "New York", "Orlando", "Phoenix", "San Francisco", "Seattle",
